6c80f553e4942106ed20431b029b672cafc52935
cwong2015 <sirhc8002@gmail.com>
2017-08-03 18:03:05 -0400

    styled more and implemented some eventhandling

--- js/index.js
+++ js/index.js
@@ -1,6 +1,27 @@
-var buttons = ["breakAdd", "breakSub", "sessionAdd", "sessionSub"];
-var symbols = ["-", "+"];
-var buttonClasses = ["buttonSub", "buttonAdd"];
+//buttonIDs: "breakAdd", "breakSub", "sessionAdd", "sessionSub"
+
+var breakLength = 5;    // 5 minutes
+var sessionLength = 25; // 25 minutes
 
 $(document).ready(function(){
+    $("button").click(function(){
+        if(this.id === "breakAdd" || this.id === "breakSub"){
+            if(this.id === "breakAdd"){
+                breakLength++;
+            }
+            else{
+                breakLength--;
+            }
+            $("#breakTimeset").text(breakLength);
+        }
+        else if(this.id === "sessionAdd" || this.id === "sessionSub"){
+            if(this.id === "sessionAdd"){
+                sessionLength++;
+            }
+            else{
+                sessionLength--;
+            }
+            $("#sessionTimeset").text(sessionLength);
+        }
+    });
 });
