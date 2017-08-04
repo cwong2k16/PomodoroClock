28e0d48f230010f5b566c1c2ba1164df98190738
cwong2015 <sirhc8002@gmail.com>
2017-08-03 23:40:15 -0400

    started implementing the timer, some logical errors, but general logic is there

--- js/index.js
+++ js/index.js
@@ -3,6 +3,9 @@
 var breakLength = 5;    // 5 minutes
 var sessionLength = 25; // 25 minutes
 var playing = false;    // toggle this for playing/pausing
+var currTimeSess = -1;
+var currTimeBreak = -1;
+var inBreak = false;;
 
 $(document).ready(function(){
     $("button").click(function(){
@@ -25,13 +28,47 @@ $(document).ready(function(){
             $("#sessionTimeset").text(sessionLength);
         }
         else{
-//            playing = !playing;
-//            if(playing){
-//                $("#playpause").attr('src', '/assets/pause.png');
-//            }
-//            else{
-//                $("#playpause").attr('src', '/assets/play.png');
-//            }
+            playing = !playing;
+            if(playing){
+                $("#playpause").attr('src', '/assets/pause.png');
+                play();
+            }
+            else{
+                $("#playpause").attr('src', '/assets/play.png');
+                pause();
+            }
         }
     });
 });
+
+function play(){
+    if(!inBreak){
+        var additMillis = (sessionLength * 60) * 1000; // convert user's desired minutes to seconds, and then to milliseconds
+        var destinationTime = (new Date().getTime()) + additMillis;
+        var difference = destinationTime - (new Date()).getTime();
+        if(difference > 0){
+            setInterval(function(){
+                difference = destinationTime - (new Date()).getTime();
+                console.log("SESSION: " + difference);
+            }, 1000);
+        }
+        else{
+            inBreak = true;
+        }
+    }
+    else{
+        var additMillis = (breakLength * 60) * 1000; // convert user's desired minutes to seconds, and then to milliseconds
+        var destinationTime = (new Date().getTime()) + additMillis;
+        var difference = destinationTime - (new Date()).getTime();
+        if(difference > 0){
+            setInterval(function(){
+                difference = destinationTime - (new Date()).getTime();
+                console.log("BREAK: " + difference);
+            }, 1000);
+        }
+        else{
+            inBreak = false;
+        }
+    }
+}
+
