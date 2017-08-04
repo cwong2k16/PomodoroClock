c64d88c9b34d48b04a9239759225bf10f0827fe2
cwong2015 <sirhc8002@gmail.com>
2017-08-04 10:21:33 -0400

    successfully implemented playing/pausing, removed a lot of redudant code

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@
 
 var breakLength = 5;    // 5 minutes
 var sessionLength = 25; // 25 minutes
-var breakCopy = breakLength;
-var sessionCopy = sessionLength;
+var breakCopy = breakLength * 60000; // convert minutes to milliseconds 
+var sessionCopy = sessionLength * 60000; // convert minutes to milliseconds
 var playing = false;    // toggle this for playing/pausing
 var currTimeSess = -1;
 var currTimeBreak = -1;
@@ -15,103 +15,60 @@ $(document).ready(function(){
         if(this.id === "breakAdd" || this.id === "breakSub"){
             if(this.id === "breakAdd"){
                 breakLength++;
-                breakCopy = breakLength;
+                breakCopy = breakLength*60000;
             }
             else{
                 breakLength--;
-                breakCopy = breakLength;
+                breakCopy = breakLength * 60000;
             }
             $("#breakTimeset").text(breakLength);
         }
         else if(this.id === "sessionAdd" || this.id === "sessionSub"){
             if(this.id === "sessionAdd"){
                 sessionLength++;
-                sessionCopy = sessionLength;
+                sessionCopy = sessionLength * 60000;
             }
             else{
                 sessionLength--;
-                sessionCopy = sessionLength;
+                sessionCopy = sessionLength * 60000;
             }
             $("#sessionTimeset").text(sessionLength);
         }
         else{
             playing = !playing;
-            if(playing){
-                $("#playpause").attr('src', '/assets/pause.png');
-                play();
-            }
-            else{
-                $("#playpause").attr('src', '/assets/play.png');
-                pause();
-            }
+            $("#playpause").attr('src', '/assets/pause.png');
+            play();
         }
     });
 });
 
-//if(!inBreak){
-//    var additMillis = (sessionLength * 60) * 1000; // convert user's desired minutes to seconds, and then to milliseconds
-//    var destinationTime = (new Date().getTime()) + additMillis;
-//    var difference = destinationTime - (new Date()).getTime();
-//    if(difference > 0){
-//        setInterval(function(){
-//            difference = destinationTime - (new Date()).getTime();
-//            console.log("SESSION: " + difference);
-//        }, 1000);
-//    }
-//    else{
-//        inBreak = true;
-//    }
-//}
-
 function play(){
-    var additMillis;
-    var destinationTime;
-    var difference;
+    if(playing)
+        interval = setInterval(tick, 1000);
+    else
+        clearInterval(interval);
+}
+
+function tick(){
     if(!inBreak){
-        additMillis = (sessionCopy * 60) * 1000; // convert user's desired minutes to seconds, and then to milliseconds
-        destinationTime = (new Date().getTime()) + additMillis;
-        difference = destinationTime - (new Date()).getTime();
-    }
-    else {
-        additMillis = (breakCopy * 60) * 1000; // convert user's desired minutes to seconds, and then to milliseconds
-        destinationTime = (new Date().getTime()) + additMillis;
-        difference = destinationTime - (new Date()).getTime();
-    }
-    
-    var diff = difference;
-    interval = setInterval(function(){
-        if(!inBreak){
-            additMillis = sessionCopy*1000;
-            destinationTime = (new Date().getTime()) + additMillis;
-            diff = destinationTime - (new Date()).getTime();
-            if(diff > 0){
-                console.log("SESSION: " + diff + " " + sessionCopy);
-                sessionCopy--;
-                additMillis = sessionCopy*1000;
-                destinationTime = (new Date().getTime()) + additMillis;
-                diff = destinationTime - (new Date()).getTime();
-            }
-            else{
-                inBreak = true;
-                sessionCopy = sessionLength;
-            }
+        if(sessionCopy >= 0){
+            console.log("SESSION: " + sessionCopy);
+            sessionCopy -= 1000;
         }
         else{
-            additMillis = breakCopy * 1000; // convert user's desired minutes to seconds, and then to milliseconds
-            destinationTime = (new Date().getTime()) + additMillis;
-            diff = destinationTime - (new Date()).getTime();
-            if(diff > 0){
-                console.log("BREAK: " + diff);
-                breakCopy--;
-                additMillis = breakCopy * 1000; // convert user's desired minutes to seconds, and then to milliseconds
-                destinationTime = (new Date().getTime()) + additMillis;
-                diff = destinationTime - (new Date()).getTime();
-            }
-            else{
-                inBreak = false;
-                breakCopy = breakLength;
-            }            
+            inBreak = true;
+            sessionCopy = sessionLength*60000;
         }
-    }, 1000);
+    }
+    else{
+        if(breakCopy >= 0){
+            console.log("BREAK: " + breakCopy);
+            breakCopy-=1000;
+        }
+        else{
+            inBreak = false;
+            breakCopy = breakLength*60000;
+        }            
+    }
 }
 
